077cedf0ccbc53d08c63216b8a6030e26a770d03
Esther <esthertrandev@gmail.com>
2026-05-23 14:50:20 -0600

    test: add CreateAccount unit test

--- db/query/account.sql
+++ db/query/account.sql
@@ -4,7 +4,25 @@ INSERT INTO accounts (
     balance,
     currency
 ) VALUES (
-    'alice',
-    100,
-    'USD'
+    $1, $2, $3
 ) RETURNING *;
+
+-- name: GetAccount :one
+SELECT * FROM accounts
+WHERE id = $1 LIMIT 1;
+
+-- name: ListAccounts :many
+SELECT * FROM accounts
+ORDER BY id
+LIMIT $1
+OFFSET $2;
+
+-- name: UpdateAccount :one
+UPDATE accounts
+SET balance = $2
+WHERE id = $1
+RETURNING *;
+
+-- name: DeleteAccount :exec
+DELETE FROM accounts
+WHERE id = $1;
