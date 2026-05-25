f0d6d07bf5f98d3719c50a36795e000d15bdecc9
Esther <esthertrandev@gmail.com>
2026-05-25 00:39:23 -0600

    feat: add db transaction

---
+++ db/sqlc/transfer_test.go
@@ -0,0 +1,26 @@
+package db
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestCreateTransfer(t *testing.T) {
+	account1 := createRandomAccount(t)
+	account2 := createRandomAccount(t)
+	arg := CreateTransferParams{
+		FromAccountID: account1.ID,
+		ToAccountID:   account2.ID,
+		Amount:        1,
+	}
+	transfer, err := testQueries.CreateTransfer(context.Background(), arg)
+	require.NoError(t, err)
+	require.NotEmpty(t, transfer)
+	require.Equal(t, transfer.FromAccountID, arg.FromAccountID)
+	require.Equal(t, transfer.ToAccountID, arg.ToAccountID)
+	require.Equal(t, transfer.Amount, arg.Amount)
+	require.NotZero(t, transfer.ID)
+	require.NotZero(t, transfer.CreatedAt)
+}
