57a4a51dd9fce5b0c6916942c9362d6570876628
Esther <esthertrandev@gmail.com>
2026-05-27 02:43:41 -0600

    feat: add POST and GET account route

---
+++ api/server.go
@@ -0,0 +1,30 @@
+package api
+
+import (
+	db "github.com/estherdev03/simple-bank/db/sqlc"
+	"github.com/gin-gonic/gin"
+)
+
+// Server serves HTTP requests for our banking service
+type Server struct {
+	store  *db.Store
+	router *gin.Engine
+}
+
+// NewServer creates a new HTTP server and set up routing
+func NewServer(store *db.Store) *Server {
+	server := &Server{store: store}
+	router := gin.Default()
+	router.POST("/accounts", server.createAccount)
+	router.GET("/accounts/:id", server.getAccount)
+	server.router = router
+	return server
+}
+
+func (server *Server) Start(address string) error {
+	return server.router.Run(address)
+}
+
+func errorResponse(err error) gin.H {
+	return gin.H{"error": err.Error()}
+}
